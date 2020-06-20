b0b9e4428f292900cff005e800afe941ed14ed09
austint33 <austintumlinson@gmail.com>
2020-06-20 14:51:15 -0500

    creating store using modern redux/toolkit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const middleware = [
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+];
+
+const authState = {
+  token: "",
+};
+
+function authReducer(state = authState, action) {
+  //code here
+  return state;
+}
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+  middleware,
+});
