dbca2d921ecf35a62a9b6991ce8c1e1aa3b7f3aa
austint33 <austintumlinson@gmail.com>
2020-06-20 21:00:22 -0500

    finished tutorial working with modern redux toolkit

--- src/index.js
+++ src/index.js
@@ -26,6 +26,36 @@ const authSlice = createSlice({
   },
 });
 
+const getUsers = createAsyncThunk("users/getUsers", () => {
+  return fetch("api/users")
+    .then(res => {
+      if(!res.ok) throw Error(res.statusText);
+      return res.json();
+    })
+    .then(json => json);
+});
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: {
+    loading: "",
+    error: "",
+    data: [],
+  },
+  reducers: {
+    //regular reducers
+  },
+  extraReducers: {
+    [getUsers.pending]: state => {
+      state.loading = "yes";
+    },
+    [getUsers.fulfilled]: (state, action) => {
+      state.loading = "";
+      state.data = action.payload;
+    }
+  }
+});
+
 const { loginSuccess, loginFailed } = authSlice.actions;
 const authReducer = authSlice.reducer;
 
